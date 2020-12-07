3620a3d7026f9902d9a0722405e7ae2024ba1d21
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2020-12-07 21:09:20 +1000

    More functions

--- .Rproj.user/73FFFEDC/sources/s-BA481FD0/31EC6647-contents
+++
@@ -1,4 +0,0 @@
-
-require(hotteR)
-
-the_plays <- get_plays(year = 2020)
---
+++ R/do_plays_analysis.R
@@ -0,0 +1,83 @@
+#'
+#' Function to produce a small analysis document for a given selection of song plays based on tweets
+#'
+#' @import dplyr
+#' @importFrom magrittr %>%
+#' @importFrom janitor clean_names
+#' @import ggplot2
+#' @author Trent Henderson
+#' @export
+#' @param data The dataframe of triplejplays data to analyse
+#'
+
+do_tweet_analysis <- function(data){
+
+  message("Running analysis...")
+
+  # Check if colnames of the input dataframe are correct
+
+  tmp <- data %>%
+    janitor::clean_names()
+
+  my_names <- colnames(tmp)
+  the_names <- c("hottest_100_year", "artist", "song", "plays")
+
+  if(setequal(my_names, the_names) == FALSE){
+    stop("Input dataframe should have the following column structure: ")
+  }
+
+  #--------------------- Analysis and data visualisation -----------
+
+  # Artist breakdown
+
+  p1 <- tmp %>%
+    dplyr::group_by(artist, plays) %>%
+    dplyr::summarise(counter = n()) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(indicator = ifelse(counter <= 3,artist,"Artists with < 3 plays")) %>%
+    dplyr::group_by(artist, indicator) %>%
+    dplyr::summarise(plays = sum(plays)) %>%
+    dplyr::ungroup() %>%
+    ggplot2::ggplot(aes(x = reorder(indicator, plays), y = plays)) +
+    ggplot2::geom_bar(stat = "identity", fill = "#393F5F") +
+    ggplot2::labs(title = "Artist breakdown",
+                  x = "Artist",
+                  y = "Number of plays") +
+    ggplot2::coord_flip() +
+    ggplot2::theme(panel.grid.minor = element_blank())
+
+  # Artist-by-song breakdown
+
+  tmp_song_artist <- tmp %>%
+    dplyr::group_by(artist) %>%
+    dplyr::summarise(counter = n()) %>%
+    dplyr::ungroup()
+
+  tmp_song_plays <- tmp %>%
+    dplyr::group_by(artist, plays) %>%
+    dplyr::summarise(counter = sum(plays)) %>%
+    dplyr::ungroup()
+
+  tmp_song <- tmp_song_artist %>%
+    dplyr::left_join(tmp_song_plays, by = c("artist" = "artist"))
+
+  if(nrow(tmp_song) == 0){
+    stop("An error occurred. No data found to plot.")
+  }
+
+  p1 <- tmp_song %>%
+    ggplot2::ggplot(aes(x = number_of_songs, y = number_of_plays)) +
+    ggplot2::geom_point(size = 2, alpha = 0.8, colour = "#393F5F") +
+    ggplot2::labs(title = "Artist-by-song breakdown",
+                  x = "Number of songs played",
+                  y = "Total plays",
+                  caption = "Each point is for an artist.") +
+    ggplot2::coord_flip() +
+    ggplot2::theme(panel.grid.minor = element_blank())
+
+  #--------------------- Merge into one plot and export ------------
+
+  p2 <- ggpubr::ggarrange(p, p1, nrows = 1, ncols = 2)
+
+  return(p2)
+}
